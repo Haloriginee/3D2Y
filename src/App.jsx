8735a6525517d660b819f73832add5d8c66dd1a9
Kevin Hang <herezy2006@gmail.com>
2023-11-16 04:34:49 +0100

    added skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { Home, Projects, Contact } from "./pages";
 
 const App = () => {
   return (
@@ -8,9 +9,9 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
-          <Route path="/projects" element={<div>Projects</div>} />
-          <Route path="/contact" element={<div>Contact</div>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
