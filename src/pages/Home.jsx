48944444f9e42bc8005e9d0f670210286aaf3613
Kevin Hang <herezy2006@gmail.com>
2023-11-20 05:16:38 +0100

    motion implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import  Loader  from '../components/Loader';
 import  Island  from '../models/Island';
 import  Sky  from '../models/Sky';
 import  Bird  from '../models/Bird';
+import  Plane  from '../models/Plane';
 
 const Home = () => {
+
+  const [isRotating, setIsRotating] = useState(false);
 
   const custom = () => {
     let screenScale = null;
@@ -21,7 +24,21 @@ const Home = () => {
     return [screenScale, screenPosition, rotation]
   }
 
+  const plane = () => {
+    let screenScale, screenPosition ;
+
+    if(window.innerWidth < 768) {
+      screenScale= [1.5, 1.5, 1.5];
+      screenPosition= [0, -1.5, 0];
+    } else {
+      screenScale= [3, 3, 3];
+      screenPosition= [0, -4, -4];
+    }
+    return [screenScale, screenPosition]
+  }
+
   const [islandScale, islandPosition, islandRotation] = custom();
+  const [ planeScale, planePosition ] = plane();
 
   return (
 
@@ -33,7 +50,7 @@ const Home = () => {
 
       <Canvas
         camera={{ near: 0.1, far: 1000}}
-        className='w-full h-screen bg-transparent'
+        className='w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab" }'
       >
         <Suspense fallback={<Loader />}>
           <ambientLight />
@@ -49,7 +66,17 @@ const Home = () => {
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
           />
+
+          <Plane
+            planeScale={planeScale}
+            planePosition={planePosition}
+            isRotating={isRotating}
+            rotation={[0, 20, 0]}
+          />
+
         </Suspense>
       </Canvas>
 
