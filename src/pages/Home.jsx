cd824a1e2bf4b8a10bad2e16dbc61c7111be88d9
Kevin Hang <herezy2006@gmail.com>
2023-11-28 14:38:34 +0100

    better plane positioning

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -76,8 +76,8 @@ const Home = () => {
           />
 
           <Plane
-            planeScale={planeScale}
-            planePosition={planePosition}
+            scale={planeScale}
+            position={planePosition}
             isRotating={isRotating}
             rotation={[0, 20, 0]}
           />
