d5c189d9abd80e07310f97381b0d90ee1d82ea7d
Kevin Hang <herezy2006@gmail.com>
2024-04-03 08:26:42 +0200

    project added

---
+++ src/constants/index.js
@@ -0,0 +1,71 @@
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript
+} from "../assets/icons";
+
+export const projects = [
+  {
+      iconUrl: pricewise,
+      theme: 'btn-back-red',
+      name: 'NhaQueMovie',
+      description: "Get ready to binge-watch your heart out with our Netflix clone! It's like having a never-ending buffet of movies and TV shows at your fingertips. With personalized recommendations, multi-user profiles, and a massive library of titles, you'll never run out of options.",
+      link: 'https://nhaquemoviev2.netlify.app/',
+  },
+  {
+      iconUrl: threads,
+      theme: 'btn-back-green',
+      name: 'NhaQueMusic',
+      description: "Get ready to jam out with our Spotify clone! It's like having a personal DJ in your pocket. With millions of songs at your fingertips, you can listen to all your favorites and discover new music to love. Our playlists are so good, you'll wonder how you ever lived without them.",
+      link: "https://nhaquemusics-v2.netlify.app/",
+  },
+  {
+      iconUrl: car,
+      theme: 'btn-back-blue',
+      name: 'HypeWheels',
+      description: "Rev up your adventure with our car renting app! Buckle up and choose from a fleet of wheels that'll make your heart race. Whether you're cruising for a day or embarking on a road trip, we've got the keys to your dream ride. Hit the road to experience the thrill of ultimate freedom on four wheels!",
+      link: 'https://hypewheels.netlify.app/',
+  },
+  {
+      iconUrl: snapgram,
+      theme: 'btn-back-pink',
+      name: 'IMGen',
+      description: "Unleash your imagination with our AI image generator! From surreal landscapes to funky patterns, our app generates unique and vibrant images that will blow your mind. Get ready to explore a world of endless possibilities and let our AI take your creativity to the next level.",
+      link: "https://imgene.netlify.app/",
+  },
+  {
+      iconUrl: estate,
+      theme: 'btn-back-black',
+      name: '"Vroom-Vroom',
+      description: "Get ready to take your car customization game to the next level with our cutting-edge AI-powered web-app!  With just a few clicks, you can transform your ride from a plain jane to a head-turning masterpiece. Let our AI be your co-pilot as you transform your car into a work of art.",
+      link: 'https://cusawroad.netlify.app/',
+  },
+  {
+      iconUrl: summiz,
+      theme: 'btn-back-yellow',
+      name: "Fake & Famous",
+      description: "Prepare to unleash your inner speed demon! Step into the driver's seat of sleek, futuristic cars and race in a neon-infused world. Feel the rush as you navigate gravity-defying loops, blazing straightaways, and treacherous twists, all while leaving a trail of dazzling light in your wake.",
+      link: "https://gamepholife.netlify.app/"
+  }
+];
